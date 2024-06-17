8c4e1c80e92dfbaebca4786e41fafb0e32e4712b
Gabriel Bento <gab.silva.bento@gmail.com>
2024-06-17 15:24:57 -0300

    Funcoes Matematicas

---
+++ src/funcoes/math/FuncoesMatematicas.java
@@ -0,0 +1,10 @@
+package funcoes.math;
+
+public class FuncoesMatematicas {
+    public static void main(String[] args){
+        double a = Math.sqrt(25); // raiz quadrada
+        double b = Math.pow(9, 2); // potencia
+        double c = Math.abs(-15); // absoluto
+        System.out.println("a:"+a+"\nb:"+b+"\nc:"+c);
+    }
+}
