3b470bb4c36be7b51082335a38dc94a2a6469ce9
Gabriel Bento <gab.silva.bento@gmail.com>
2024-06-17 15:19:22 -0300

    entrada de dados completa

--- src/entradaDados/Entrada.java
+++ src/entradaDados/Entrada.java
@@ -7,8 +7,27 @@ public class Entrada {
     public static void main(String[] args){
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um double");
         double a = sc.nextDouble();
         System.out.println(a);
+        System.out.println("Digite um char");
+        char x = sc.next().charAt(0);
+        System.out.println("Digite um texto");
+        String y = sc.next();
+        System.out.println("Digite um inteiro");
+        int e = sc.nextInt();
+        // Quebra de Linha
+        sc.nextLine();
+        String s1, s2, s3;
+        System.out.println("Digite Texto");
+        s1 = sc.nextLine();
+        System.out.println("Digite Texto");
+        s2 = sc.nextLine();
+        System.out.println("Digite Texto");
+        s3 = sc.nextLine();
+        System.out.println("s1:"+s1+"\ns2:"+s2+"\ns3:"+s3);
+        // quebra de linha pendente
         sc.close();
+
     }
 }
