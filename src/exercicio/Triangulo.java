c2919fad436e15ce356febda2d2edafbaead2a01
Gabriel Bento <gab.silva.bento@gmail.com>
2024-06-22 12:38:09 -0300

    adicionados metodos para calcular a area

--- src/exercicio/Triangulo.java
+++ src/exercicio/Triangulo.java
@@ -4,4 +4,12 @@ public class Triangulo {
     public double a;
     public double b;
     public double c;
+
+    public double semiperimetro(){
+        return (this.a + this.b + this.c) / 2.0;
+    }
+
+    public double area(){
+        return Math.sqrt(semiperimetro() * (semiperimetro() - this.a) * (semiperimetro() - this.b) * (semiperimetro() - this.c));
+    }
 }
