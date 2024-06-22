7988d9e6ff2d0aa755f54a7b883ec1cb1920fdfe
Gabriel Bento <gab.silva.bento@gmail.com>
2024-06-22 12:38:39 -0300

    alterado nome e implementado o metodo de triangulo

--- src/exercicio/Exercicio.java
+++ src/exercicio/Exercicio.java
@@ -3,7 +3,7 @@ package exercicio;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class CalculaTriangulo {
+public class Exercicio {
     public static void main(String[] args){
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
@@ -21,11 +21,9 @@ public class CalculaTriangulo {
         y.b = sc.nextDouble();
         y.c = sc.nextDouble();
 
-        double p = (x.a + x.b + x.c) / 2.0;
-        double areaX = Math.sqrt(p*(p-x.a)*(p-x.b)*(p-x.c));
+        double areaX = x.area();
 
-        p = (y.a + y.b + y.c) / 2.0;
-        double areaY = Math.sqrt(p*(p-y.a)*(p-y.b)*(p-y.c));
+        double areaY = y.area();
 
         System.out.printf("A area do triangulo x: %.4f%n",areaX);
         System.out.printf("A area do triangulo y: %.4f%n",areaY);
