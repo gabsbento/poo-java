b7b705b47b0bf802346900f51399294ebd33ccf0
gabriel <gab.silva.bento@gmail.com>
2024-06-25 15:14:02 -0300

    Criado Exemplo de objeto

---
+++ src/object/Exemplo.java
@@ -0,0 +1,12 @@
+package object;
+
+import exercicio.Triangulo;
+
+public class Exemplo {
+    public static void main(String[] args){
+
+        Triangulo triangulo = new Triangulo();
+        System.out.println(triangulo.toString());
+    }
+
+}
